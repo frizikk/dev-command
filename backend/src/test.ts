94c4f812dd072dac118b0e90b156ed3a9bf6d288
DevCommand Assistant <devcommand@example.com>
2026-02-09 21:00:07 +0100

    Cleanup: remove debug test file

--- backend/src/test.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => res.send('OK'));
-app.listen(3001, () => {
-    console.log('Test server on 3001');
-});
-setInterval(() => { }, 1000); // Force keep-alive
